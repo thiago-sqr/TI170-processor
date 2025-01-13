acd77d096350a97f62e5e1635cb795f352a63a20
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2025-01-13 17:29:57 -0300

    Update ALU.v

--- modules/ALU.v
+++ modules/ALU.v
@@ -9,21 +9,21 @@ module ALU (
 
     always @(*) begin
         case (ALU_Sel)
-            4'b0000: begin  // Soma
+            4'h0: begin  // Soma
                 {NZVC[0], Result} = A + B;  // Soma e Carry Flag
                 NZVC[3] = Result[7];        // Negative Flag
                 NZVC[2] = (Result == 8'h00) ? 1 : 0;  // Zero Flag
                 NZVC[1] = ((A[7] == B[7]) && (A[7] != Result[7])) ? 1 : 0;  // Overflow Flag
             end
 
-            4'b0001: begin  // Subtração
+            4'h1: begin  // Subtração
                 {NZVC[0], Result} = A - B;
                 NZVC[3] = Result[7];
                 NZVC[2] = (Result == 8'h00) ? 1 : 0;
                 NZVC[1] = ((A[7] != B[7]) && (A[7] != Result[7])) ? 1 : 0;
             end
 
-            4'b0010: begin  // Multiplicação
+            4'h2: begin  // Multiplicação
                 TempResult = A * B;
                 Result = TempResult[7:0];
                 NZVC[3] = Result[7];
@@ -32,7 +32,7 @@ module ALU (
                 NZVC[0] = 0;  // Carry Flag não se aplica
             end
 
-            4'b0011: begin  // Divisão
+            4'h3: begin  // Divisão
                 if (B != 0) begin
                     Result = A / B;
                     NZVC[3] = Result[7];
@@ -41,22 +41,24 @@ module ALU (
                     NZVC[0] = 0;  // Carry não se aplica
                 end else begin
                     Result = 8'hFF;  // Indicador de erro para divisão por zero
-                    NZVC = 4'b1111;  // Flags indicativas de erro
+                    NZVC = 4'hF;  // Flags indicativas de erro
                 end
             end
 
-            4'b0100: begin  // Resto da divisão (Módulo)
+            4'h4: begin  // Resto da divisão (Módulo)
                 if (B != 0) begin
                     Result = A % B;
                     NZVC[3] = Result[7];
                     NZVC[2] = (Result == 8'h00) ? 1 : 0;
+                    NZVC[1] = 0;  // Overflow não se aplica
+                    NZVC[0] = 0;  // Carry não se aplica
                 end else begin
                     Result = 8'hFF;
-                    NZVC = 4'b1111;
+                    NZVC = 4'hF;
                 end
             end
 
-            4'b0101: begin  // Comparação (A == B)
+            4'h5: begin  // Comparação (A == B)
                 Result = 8'h00;  // Resultado nulo para comparação
                 NZVC[3] = 0;
                 NZVC[2] = (A == B) ? 1 : 0;
@@ -64,10 +66,10 @@ module ALU (
                 NZVC[0] = 0;
             end
 
-            4'b0110: Result = A & B;  // E (AND)
-            4'b0111: Result = A | B;  // OU (OR)
-            4'b1000: Result = ~A;     // Negacao (NOT)
-            4'b1001: Result = A ^ B;  // OU-exclusivo (XOR)
+            4'h6: Result = A & B;  // E (AND)
+            4'h7: Result = A | B;  // OU (OR)
+            4'h8: Result = ~A;     // Negacao (NOT)
+            4'h9: Result = A ^ B;  // OU-exclusivo (XOR)
 
             default: begin
                 Result = 8'hXX;
@@ -76,7 +78,7 @@ module ALU (
         endcase
 
         // Atualização de Flags pós-operações lógicas, quando aplicável
-        if (ALU_Sel >= 4'b0110 && ALU_Sel <= 4'b1001) begin
+        if (ALU_Sel >= 4'h6 && ALU_Sel <= 4'h9) begin
             NZVC[3] = Result[7];                 // Negative Flag
             NZVC[2] = (Result == 8'h00) ? 1 : 0; // Zero Flag
             NZVC[1] = 0;                         // Overflow não se aplica
