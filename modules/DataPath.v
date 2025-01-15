82934d30343307a53ef06b1b09780d44e20019f3
MRGdSFS <130089424+MRGdSFS@users.noreply.github.com>
2025-01-14 23:41:37 -0300

    Update DataPath.v
    Louco e sonhodor, vapo que vapo no trapo do talo
    Quem leu é inteligente
    (incompleto, eu acho)

--- modules/DataPath.v
+++ modules/DataPath.v
@@ -1,10 +1,10 @@
 module caminho_dados (
     input wire clock, reset,
     input wire [1:0] Bus1_Sel, Bus2_Sel,
-    input wire PC_Load, PC_Inc, A_Load, B_Load, IR_Load, MAR_Load, CCR_Load,
+    input wire PC_Load, PC_Inc, A_Load, B_Load, C_Load, IR_Load, MAR_Load, CCR_Load,
     input wire [7:0] ALU_Result, from_memory, NZVC,
     output reg [7:0] to_memory, address,
-    output reg [7:0] IR, A, B, PC, MAR, CCR_Result
+    output reg [7:0] IR, A, B, C, PC, MAR, CCR_Result
 );
 
     // Barramentos
@@ -16,6 +16,7 @@ module caminho_dados (
             2'b00: Bus1 = PC;
             2'b01: Bus1 = A;
             2'b10: Bus1 = B;
+            2'b11: Bus1 = C;
             default: Bus1 = 8'hXX;
         endcase
     end
@@ -23,9 +24,10 @@ module caminho_dados (
     // Multiplexador para Bus2
     always @(*) begin
         case (Bus2_Sel)
-            2'b00: Bus2 = ALU_Result;
-            2'b01: Bus2 = Bus1;
+            2'b00: Bus2 = Bus1;
+            2'b01: Bus2 = 8'h01;
             2'b10: Bus2 = from_memory;
+            2'b11: Bus2 = ALU_Result;
             default: Bus2 = 8'hXX;
         endcase
     end
@@ -59,10 +61,10 @@ module caminho_dados (
         else if (PC_Load)
             PC <= Bus2;
         else if (PC_Inc)
-            PC <= MAR + 1;
+            PC <= PC + 1;
     end
 
-    // Registradores A e B
+    // Registradores A, B e C
     always @(posedge clock or negedge reset) begin
         if (!reset)
             A <= 8'h00;
@@ -75,6 +77,13 @@ module caminho_dados (
             B <= 8'h00;
         else if (B_Load)
             B <= Bus2;
+    end
+
+    always @(posedge clock or negedge reset) begin
+        if(!reset)
+            C <= 8'h00;
+        else if (C_Load)
+            C <= Bus2;
     end
 
     // Registrador de Códigos de Condição (CCR)
